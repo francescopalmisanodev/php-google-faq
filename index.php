e3d575f6a6a45273969e825f5ef0c6a4fb282359
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-12-20 16:52:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>FAQ</title>
+</head>
+
+<body>
+    <header>
+        <div class="row">
+            <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Google">
+            <h2>Privacy e termini</h2>
+        </div>
+        <ul>
+            <li>
+                <h3>Introduzione</h3>
+            </li>
+            <li>
+                <h3>Norme sulla privacy</h3>
+            </li>
+            <li>
+                <h3>Termini di servizio</h3>
+            </li>
+            <li>
+                <h3>Tecnologie</h3>
+            </li>
+            <li>
+                <h3>Domande Frequenti</h3>
+            </li>
+        </ul>
+        <hr>
+    </header>
+    <main>
+        <h2></h2>
+        <p></p>
+    </main>
+</body>
+
+</html>
